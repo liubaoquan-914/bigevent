23e92c0bf86dfffeff5b1157fc7487050177d3a6
liubaoquan <liubaoquan20201117@163.com>
2020-11-21 11:32:03 +0800

    后台主页完成

--- assets/js/baseAPI.js
+++ assets/js/baseAPI.js
@@ -4,4 +4,17 @@
 $.ajaxPrefilter(function (options) {
     // 在发起真正的 Ajax 请求之前，统一拼接请求的根路径
     options.url = 'http://ajax.frontend.itheima.net' + options.url
+    //在调用权限接口的时候  指定complete回调函数  ajax自带的complete
+    //全局统一挂载complate回调函数
+    options.complete = function (res) {
+        // console.log('执行了 complete 回调：')
+        // console.log(res)
+        // 在 complete 回调函数中，可以使用 res.responseJSON 拿到服务器响应回来的数据  注意'身份认证失败 '后面必须有空格
+        if (res.responseJSON.status !== 1 && res.responseJSON.message === '身份认证失败! ') {
+            //强制清空token
+            localStorage.removeItem('token')
+            //强制跳转到login页面
+            location.href = '/login.html'
+        }
+    }
 })
---
+++ assets/js/index.js
@@ -0,0 +1,57 @@
+$(function () {
+    getUserInfo()
+    //实现退出功能
+    $('#btnLogout').on('click', function () {
+        // console.log(12);
+        layui.layer.confirm('确定退出吗?', {
+            icon: 3,
+            title: '提示'
+        }, function (index) {
+            //do something
+            //点击确定之后清除本地的token  注意不能用clear  clear会把所有的都清除掉
+            localStorage.removeItem('token')
+            //跳转到longin.html
+            location.href = '/login.html'
+            //关闭询问框  layui自带 
+            layer.close(index);
+        });
+    })
+})
+//获取用户的基本信息
+function getUserInfo() {
+    $.ajax({
+        method: "GET",
+        url: "/my/userinfo",
+        //headers就是请求头配置对象
+        headers: {
+            Authorization: localStorage.getItem('token') || ''
+        },
+        success: function (res) {
+            console.log(res);
+            if (res.status !== 0) {
+                return layui.layer.msg('获取用户信息失败!')
+            }
+            // 调用  renderAvatar 渲染头部头像的函数
+            renderAvatar(res.data)
+        }
+    });
+}
+//渲染头部头像
+function renderAvatar(user) {
+    //获取用户名
+    var name = user.nickname || user.username;
+    //设置欢迎文本
+    $('#welcome').html("欢迎&nbsp&nbsp" + name)
+    //按用户需要渲染头部头像
+    //如果有图片就显示图片头像
+    if (user.user_pic !== null) {
+        $('#layui-nav-img').attr('src', user.user_pic).show()
+        $('.items').hide()
+    } else {
+        //如果没有图片就显示名字的第一个文字
+        $('#layui-nav-img').hide()
+        var first = name[0].toUpperCase()
+        $('.items').html(first).show()
+    }
+
+}
